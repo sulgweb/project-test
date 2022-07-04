46f76f191954f6a80197833a7df27ea59d0e8d5f
sulgweb <xiaoyu@sulg.top>
2022-07-04 21:15:36 +0800

    ✨ 完成webworkerdemo

--- webworker/worker.js
+++ webworker/worker.js
@@ -1,23 +1,30 @@
-// worker 线程
+/*
+ * @Author: xiaoyu
+ * @Description: worker 线程
+ * @Date: 2022-05-08 08:41:30
+ * @LastEditors: xiaoyu
+ * @LastEditTime: 2022-06-29 23:17:44
+ */
 
+// 方法对象
 const funcObj = {
   fb: (n) => {
-    if(n===1 || n ===2){
+    if (n === 1 || n === 2) {
       return 1;
     }
-    return funcObj.fb(n-1) + funcObj.fb(n-2)
-  }
-}
+    return funcObj.fb(n - 1) + funcObj.fb(n - 2);
+  },
+};
 
+// onmessage事件
+onmessage = function (e) {
+  const { data } = e;
+  const res = funcObj[data.function](data.data);
 
-onmessage = function(e){
-  const {data} = e;
-  
-  const res = funcObj[data.function](data.data)
-  
+  // 将获取的数据通过postMessage发送到主线程
   self.postMessage({
     data: res,
-    name: 'worker test'
-  })
-  self.close()
-}
+    name: "worker test",
+  });
+  self.close();
+};
